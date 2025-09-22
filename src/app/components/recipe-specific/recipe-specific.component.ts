53765c64011500349fe8418b9140909b5e1843f0
SRsoz <sozyar.rauf@chasacademy.se>
2025-09-22 15:05:22 +0200

    add standalone

--- src/app/components/recipe-specific/recipe-specific.component.ts
+++ src/app/components/recipe-specific/recipe-specific.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-specific',
   imports: [],
+  standalone: true,
   templateUrl: './recipe-specific.component.html',
   styleUrl: './recipe-specific.component.css'
 })
